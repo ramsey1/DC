dddc331a7b030b12255bb12c311eb938e5de97a6
Mukul Verma <35653163+ramsey1@users.noreply.github.com>
2019-01-13 23:36:58 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+//  	function readURL(input) {
+//     if (input.files && input.files[0]) {
+//         var reader = new FileReader();
+
+//         reader.onload = function (e) {
+//             document.getElementById('blah').src =  e.target.result;
+//         }
+
+//         reader.readAsDataURL(input.files[0]);
+//     }
+// }
+
+
+
+function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            
+            reader.onload = function (e) {
+                $('#blah').attr('src', e.target.result);
+            }
+            
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+    $("#imgInp").change(function(){
+        readURL(this);
+    });
